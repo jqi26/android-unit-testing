7920fa761e5bd6157c04568354e50254993ea96e
Jackie Qiu <jqi26@uclive.ac.nz>
2023-01-19 15:54:31 +1300

    Can now add a new option.

--- app/src/main/java/com/example/androidunittesting/OptionListFragment.kt
+++ app/src/main/java/com/example/androidunittesting/OptionListFragment.kt
@@ -1,15 +1,22 @@
 package com.example.androidunittesting
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class OptionListFragment : Fragment() {
+    private var options = arrayListOf(Option("Test Option 1"), Option("Test Option 2"))
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,6 +35,34 @@ class OptionListFragment : Fragment() {
         )
         recyclerView.addItemDecoration(dividerItemDecoration)
 
-        recyclerView.adapter = OptionListAdapter(arrayListOf(Option("Test Option 1"), Option("Test Option 2")))
+        recyclerView.adapter = OptionListAdapter(options)
+
+        view.findViewById<FloatingActionButton>(R.id.optionListFab).setOnClickListener {
+            showNewOptionDialog()
+        }
+    }
+
+    private fun showNewOptionDialog() {
+        val builder = AlertDialog.Builder(context).setPositiveButton("Add", null)
+        val form = layoutInflater.inflate(R.layout.new_option_dialog, null, false)
+        builder.setView(form)
+
+        builder.setTitle("New Option")
+
+        val nameText = form.findViewById<EditText>(R.id.newOptionName)
+
+        builder.setNegativeButton("Cancel") { _, _ ->  }
+
+        val dialog = builder.create()
+        dialog.show()
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+            val newName = nameText.text.trim()
+            if (newName.isEmpty()) {
+                Toast.makeText(context, "Name cannot be empty.", Toast.LENGTH_SHORT).show()
+            } else {
+                options.add(Option(newName.toString()))
+                dialog.dismiss()
+            }
+        }
     }
 }
